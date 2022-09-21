13f583d4d3a4421b413785fff194e5f1f27bd957
Zach Baker <ZEBaker98@gmail.com>
2022-09-21 10:23:56 -0500

    accept input characters as argument

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,52 @@
 
+use std::env;
 use std::path::Path;
 
 mod trie;
+use trie::letters::Letters;
 
 mod test;
 use test::{time_trie_creation, time_anagram_solve};
 
+enum InputError {
+    InvalidInput(String),
+    MissingInput,
+}
+
+fn get_input() -> Result<String, InputError> {
+    let mut args = env::args();
+    match args.nth(1) {
+        Some(input) => {
+            if Letters::from_string(input.clone()).is_ok() {
+                Ok(input)
+            } else {
+                Err(InputError::InvalidInput(input))
+            }
+        },
+        None => Err(InputError::MissingInput),
+    }
+}
+
 fn main() {
-    let path = Path::new("./words_alpha.txt");
 
-    let t = time_trie_creation(path);
+    match get_input() {
+        Ok(input) => {
+            let path = Path::new("./words_alpha.txt");
 
-    let mut words = time_anagram_solve(&t, String::from("incandescent"));
-    words.sort_by_key(|x| x.len());
+            let t = time_trie_creation(path);
 
-    for word in words {
-        println!("{}", word);
+            let mut words = time_anagram_solve(&t, input);
+            words.sort_by_key(|x| x.len());
+
+            for word in words {
+                println!("{}", word);
+            }
+        },
+        Err(InputError::MissingInput) => {
+            println!("Argument is missing! Anagrams input can contain alphabetic characters and ? as wildcard characters.")
+        },
+        Err(InputError::InvalidInput(input)) => {
+            println!("Argument '{}' is invalid! Input can contain alphabetic characters and ? as wildcard characters.", input)
+        }
     }
 }
